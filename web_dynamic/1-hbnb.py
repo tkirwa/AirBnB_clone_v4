087c2a3eeed66677faa4e59215b3230c8570b7f4
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-08-06 16:25:05 +0300

    Select some Amenities to be comfortable! - 1-hbnb.py, templates/1-hbnb.html, static/scripts/1-hbnb.js

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -8,8 +8,6 @@ from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
-
-
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
@@ -37,7 +35,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
